279bce5319d1f08f28f6a494fe696e7e247ef8d6
rykcbaoolNEW <165583582+rykcbaoolNEW@users.noreply.github.com>
2026-04-28 19:39:46 -0400

    Create record.js

---
+++ record.js
@@ -0,0 +1,23 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch();
+
+  const context = await browser.newContext({
+    recordVideo: {
+      dir: 'videos/',
+      size: { width: 1280, height: 720 }
+    }
+  });
+
+  const page = await context.newPage();
+
+
+  await page.goto('https://vsembed.ru/embed/movie/687163');
+
+  await page.waitForTimeout(9392000);
+
+  await browser.close();
+
+  console.log("Recording complete");
+})();
